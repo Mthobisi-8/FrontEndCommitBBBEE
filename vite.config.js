113760a9f389ed13eec2865fd9918a4179087b67
Mthobisi Motimele <motimelemthobisi@gmail.com>
2025-07-25 11:21:34 +0200

    heart attack

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-bbbee.vercel.app/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  plugins: [react(),
+    tailwindcss()],
+    server: {
+      port: 3000
   }
-});
+})
